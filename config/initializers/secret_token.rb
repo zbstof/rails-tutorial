29ceffafce9e491b24fd15f04194ffe2af4d3504
Stanislav Bitsko <Stanislav_Bitsko@epam.com>
2013-03-13 18:27:14 +0200

    Generated controllers posts, welcome

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Prz::Application.config.secret_token = '0a1b774f961a84fb7ea888732d02826d79ce974ae3109541e87f49ab8b95aa6ee34b9fee2ded162dce6ae2d7a98266eaa5521a058c8f8af8b044de213813b1f0'
